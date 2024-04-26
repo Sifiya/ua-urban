86caa4aed2eaa0c9a8e84bee1ba18c6b7d163557
Tetyana Hrynko <sofiyasuetina@gmail.com>
2024-04-26 13:27:02 +0300

    chore(ci): add github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Тут буде словник.
+      Новий деплой з гітхабу.
     </main>
   );
 }
