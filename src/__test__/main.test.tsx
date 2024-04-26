ea09669e51e618bd3dc60110b6f2f7c17d260f69
Tetyana Hrynko <sofiyasuetina@gmail.com>
2024-04-26 18:39:39 +0300

    chore(tests): fix main page test

--- src/__test__/main.test.tsx
+++ src/__test__/main.test.tsx
@@ -5,8 +5,6 @@ import MainPage from '../app/page';
 describe('MainPage', () => {
   test('should render correctly', () => {
     render(<MainPage />);
-    const mainElement = screen.getByRole('main');
-    expect(mainElement).toBeInTheDocument();
-    expect(mainElement).toHaveTextContent('Тут буде словник. Новий деплой з гітхабу.');
+    expect(screen.getByText('Тут буде словник. Новий деплой з гітхабу.')).toBeInTheDocument();
   });
 });
