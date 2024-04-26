00f658bbe743038ca58a88685f6a0b57779a4fa5
Tetyana Hrynko <sofiyasuetina@gmail.com>
2024-04-26 15:01:54 +0300

    feat(ui): install ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
       Тут буде словник. Новий деплой з гітхабу.
+      <Button variant="default" size="lg">Кнопка</Button>
     </main>
   );
 }
